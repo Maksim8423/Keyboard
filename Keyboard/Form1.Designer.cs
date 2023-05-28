cfb1b932d264bdbc715732c21970bb27db2224b6
Maksim8423 <maksim.lebedev8423>
2023-05-28 11:30:04 +0400

    Create Keyboard

---
+++ Keyboard/Form1.Designer.cs
@@ -0,0 +1,463 @@
+﻿namespace Keyboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            qButton = new Button();
+            wButton = new Button();
+            eButton = new Button();
+            rButton = new Button();
+            tButton = new Button();
+            yButton = new Button();
+            uButton = new Button();
+            iButton = new Button();
+            oButton = new Button();
+            pButton = new Button();
+            BackspaceButton = new Button();
+            aButton = new Button();
+            sButton = new Button();
+            dButton = new Button();
+            fButton = new Button();
+            gButton = new Button();
+            hButton = new Button();
+            jButton = new Button();
+            kButton = new Button();
+            lButton = new Button();
+            QuoteButton = new Button();
+            EnterButton = new Button();
+            zButton = new Button();
+            xButton = new Button();
+            cButton = new Button();
+            bButton = new Button();
+            vButton = new Button();
+            nButton = new Button();
+            mButton = new Button();
+            CommaButton = new Button();
+            DotButton = new Button();
+            QuestionMarkButton = new Button();
+            SpaceButton = new Button();
+            Field = new TextBox();
+            SuspendLayout();
+            // 
+            // qButton
+            // 
+            qButton.Location = new Point(51, 251);
+            qButton.Name = "qButton";
+            qButton.Size = new Size(41, 23);
+            qButton.TabIndex = 0;
+            qButton.Text = "q";
+            qButton.UseVisualStyleBackColor = true;
+            qButton.Click += button1_Click;
+            // 
+            // wButton
+            // 
+            wButton.Location = new Point(115, 251);
+            wButton.Name = "wButton";
+            wButton.Size = new Size(41, 23);
+            wButton.TabIndex = 1;
+            wButton.Text = "w";
+            wButton.UseVisualStyleBackColor = true;
+            wButton.Click += button2_Click;
+            // 
+            // eButton
+            // 
+            eButton.Location = new Point(173, 251);
+            eButton.Name = "eButton";
+            eButton.Size = new Size(41, 23);
+            eButton.TabIndex = 2;
+            eButton.Text = "e";
+            eButton.UseVisualStyleBackColor = true;
+            // 
+            // rButton
+            // 
+            rButton.Location = new Point(233, 251);
+            rButton.Name = "rButton";
+            rButton.Size = new Size(41, 23);
+            rButton.TabIndex = 3;
+            rButton.Text = "r";
+            rButton.UseVisualStyleBackColor = true;
+            // 
+            // tButton
+            // 
+            tButton.Location = new Point(293, 251);
+            tButton.Name = "tButton";
+            tButton.Size = new Size(41, 23);
+            tButton.TabIndex = 4;
+            tButton.Text = "t";
+            tButton.UseVisualStyleBackColor = true;
+            // 
+            // yButton
+            // 
+            yButton.Location = new Point(361, 251);
+            yButton.Name = "yButton";
+            yButton.Size = new Size(41, 23);
+            yButton.TabIndex = 5;
+            yButton.Text = "y";
+            yButton.UseVisualStyleBackColor = true;
+            yButton.Click += button6_Click;
+            // 
+            // uButton
+            // 
+            uButton.Location = new Point(420, 251);
+            uButton.Name = "uButton";
+            uButton.Size = new Size(41, 23);
+            uButton.TabIndex = 6;
+            uButton.Text = "u";
+            uButton.UseVisualStyleBackColor = true;
+            // 
+            // iButton
+            // 
+            iButton.Location = new Point(476, 251);
+            iButton.Name = "iButton";
+            iButton.Size = new Size(41, 23);
+            iButton.TabIndex = 7;
+            iButton.Text = "i";
+            iButton.UseVisualStyleBackColor = true;
+            // 
+            // oButton
+            // 
+            oButton.Location = new Point(532, 251);
+            oButton.Name = "oButton";
+            oButton.Size = new Size(41, 23);
+            oButton.TabIndex = 8;
+            oButton.Text = "o";
+            oButton.UseVisualStyleBackColor = true;
+            oButton.Click += button9_Click;
+            // 
+            // pButton
+            // 
+            pButton.Location = new Point(591, 251);
+            pButton.Name = "pButton";
+            pButton.Size = new Size(41, 23);
+            pButton.TabIndex = 9;
+            pButton.Text = "p";
+            pButton.UseVisualStyleBackColor = true;
+            // 
+            // BackspaceButton
+            // 
+            BackspaceButton.Location = new Point(707, 251);
+            BackspaceButton.Name = "BackspaceButton";
+            BackspaceButton.Size = new Size(73, 23);
+            BackspaceButton.TabIndex = 10;
+            BackspaceButton.Text = "Backspace";
+            BackspaceButton.UseVisualStyleBackColor = true;
+            // 
+            // aButton
+            // 
+            aButton.Location = new Point(63, 291);
+            aButton.Name = "aButton";
+            aButton.Size = new Size(41, 23);
+            aButton.TabIndex = 11;
+            aButton.Text = "a";
+            aButton.UseVisualStyleBackColor = true;
+            // 
+            // sButton
+            // 
+            sButton.Location = new Point(125, 291);
+            sButton.Name = "sButton";
+            sButton.Size = new Size(41, 23);
+            sButton.TabIndex = 12;
+            sButton.Text = "s";
+            sButton.UseVisualStyleBackColor = true;
+            // 
+            // dButton
+            // 
+            dButton.Location = new Point(185, 291);
+            dButton.Name = "dButton";
+            dButton.Size = new Size(41, 23);
+            dButton.TabIndex = 13;
+            dButton.Text = "d";
+            dButton.UseVisualStyleBackColor = true;
+            // 
+            // fButton
+            // 
+            fButton.Location = new Point(248, 291);
+            fButton.Name = "fButton";
+            fButton.Size = new Size(41, 23);
+            fButton.TabIndex = 14;
+            fButton.Text = "f";
+            fButton.UseVisualStyleBackColor = true;
+            fButton.Click += button15_Click;
+            // 
+            // gButton
+            // 
+            gButton.Location = new Point(318, 291);
+            gButton.Name = "gButton";
+            gButton.Size = new Size(41, 23);
+            gButton.TabIndex = 15;
+            gButton.Text = "g";
+            gButton.UseVisualStyleBackColor = true;
+            // 
+            // hButton
+            // 
+            hButton.Location = new Point(385, 291);
+            hButton.Name = "hButton";
+            hButton.Size = new Size(41, 23);
+            hButton.TabIndex = 16;
+            hButton.Text = "h";
+            hButton.UseVisualStyleBackColor = true;
+            hButton.Click += button17_Click;
+            // 
+            // jButton
+            // 
+            jButton.Location = new Point(446, 291);
+            jButton.Name = "jButton";
+            jButton.Size = new Size(41, 23);
+            jButton.TabIndex = 17;
+            jButton.Text = "j";
+            jButton.UseVisualStyleBackColor = true;
+            // 
+            // kButton
+            // 
+            kButton.Location = new Point(508, 291);
+            kButton.Name = "kButton";
+            kButton.Size = new Size(41, 23);
+            kButton.TabIndex = 18;
+            kButton.Text = "k";
+            kButton.UseVisualStyleBackColor = true;
+            // 
+            // lButton
+            // 
+            lButton.Location = new Point(564, 291);
+            lButton.Name = "lButton";
+            lButton.Size = new Size(41, 23);
+            lButton.TabIndex = 19;
+            lButton.Text = "l";
+            lButton.UseVisualStyleBackColor = true;
+            // 
+            // QuoteButton
+            // 
+            QuoteButton.Location = new Point(631, 291);
+            QuoteButton.Name = "QuoteButton";
+            QuoteButton.Size = new Size(41, 23);
+            QuoteButton.TabIndex = 20;
+            QuoteButton.Text = "'";
+            QuoteButton.UseVisualStyleBackColor = true;
+            // 
+            // EnterButton
+            // 
+            EnterButton.Location = new Point(707, 291);
+            EnterButton.Name = "EnterButton";
+            EnterButton.Size = new Size(73, 23);
+            EnterButton.TabIndex = 21;
+            EnterButton.Text = "Enter";
+            EnterButton.UseVisualStyleBackColor = true;
+            // 
+            // zButton
+            // 
+            zButton.Location = new Point(85, 331);
+            zButton.Name = "zButton";
+            zButton.Size = new Size(41, 23);
+            zButton.TabIndex = 22;
+            zButton.Text = "z";
+            zButton.UseVisualStyleBackColor = true;
+            // 
+            // xButton
+            // 
+            xButton.Location = new Point(146, 331);
+            xButton.Name = "xButton";
+            xButton.Size = new Size(41, 23);
+            xButton.TabIndex = 23;
+            xButton.Text = "x";
+            xButton.UseVisualStyleBackColor = true;
+            // 
+            // cButton
+            // 
+            cButton.Location = new Point(209, 331);
+            cButton.Name = "cButton";
+            cButton.Size = new Size(41, 23);
+            cButton.TabIndex = 24;
+            cButton.Text = "c";
+            cButton.UseVisualStyleBackColor = true;
+            cButton.Click += button25_Click;
+            // 
+            // bButton
+            // 
+            bButton.Location = new Point(330, 331);
+            bButton.Name = "bButton";
+            bButton.Size = new Size(41, 23);
+            bButton.TabIndex = 25;
+            bButton.Text = "b";
+            bButton.UseVisualStyleBackColor = true;
+            bButton.Click += button26_Click;
+            // 
+            // vButton
+            // 
+            vButton.Location = new Point(270, 331);
+            vButton.Name = "vButton";
+            vButton.Size = new Size(41, 23);
+            vButton.TabIndex = 26;
+            vButton.Text = "v";
+            vButton.UseVisualStyleBackColor = true;
+            // 
+            // nButton
+            // 
+            nButton.Location = new Point(400, 331);
+            nButton.Name = "nButton";
+            nButton.Size = new Size(41, 23);
+            nButton.TabIndex = 27;
+            nButton.Text = "n";
+            nButton.UseVisualStyleBackColor = true;
+            // 
+            // mButton
+            // 
+            mButton.Location = new Point(464, 331);
+            mButton.Name = "mButton";
+            mButton.Size = new Size(41, 23);
+            mButton.TabIndex = 28;
+            mButton.Text = "m";
+            mButton.UseVisualStyleBackColor = true;
+            // 
+            // CommaButton
+            // 
+            CommaButton.Location = new Point(532, 331);
+            CommaButton.Name = "CommaButton";
+            CommaButton.Size = new Size(41, 23);
+            CommaButton.TabIndex = 29;
+            CommaButton.Text = ",";
+            CommaButton.UseVisualStyleBackColor = true;
+            // 
+            // DotButton
+            // 
+            DotButton.Location = new Point(600, 331);
+            DotButton.Name = "DotButton";
+            DotButton.Size = new Size(41, 23);
+            DotButton.TabIndex = 30;
+            DotButton.Text = ".";
+            DotButton.UseVisualStyleBackColor = true;
+            // 
+            // QuestionMarkButton
+            // 
+            QuestionMarkButton.Location = new Point(658, 331);
+            QuestionMarkButton.Name = "QuestionMarkButton";
+            QuestionMarkButton.Size = new Size(41, 23);
+            QuestionMarkButton.TabIndex = 31;
+            QuestionMarkButton.Text = "?";
+            QuestionMarkButton.UseVisualStyleBackColor = true;
+            // 
+            // SpaceButton
+            // 
+            SpaceButton.Location = new Point(185, 381);
+            SpaceButton.Name = "SpaceButton";
+            SpaceButton.Size = new Size(364, 23);
+            SpaceButton.TabIndex = 32;
+            SpaceButton.UseVisualStyleBackColor = true;
+            SpaceButton.Click += button33_Click;
+            // 
+            // Field
+            // 
+            Field.Location = new Point(63, 190);
+            Field.Name = "Field";
+            Field.Size = new Size(650, 23);
+            Field.TabIndex = 33;
+            Field.TextChanged += Field_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Field);
+            Controls.Add(SpaceButton);
+            Controls.Add(QuestionMarkButton);
+            Controls.Add(DotButton);
+            Controls.Add(CommaButton);
+            Controls.Add(mButton);
+            Controls.Add(nButton);
+            Controls.Add(vButton);
+            Controls.Add(bButton);
+            Controls.Add(cButton);
+            Controls.Add(xButton);
+            Controls.Add(zButton);
+            Controls.Add(EnterButton);
+            Controls.Add(QuoteButton);
+            Controls.Add(lButton);
+            Controls.Add(kButton);
+            Controls.Add(jButton);
+            Controls.Add(hButton);
+            Controls.Add(gButton);
+            Controls.Add(fButton);
+            Controls.Add(dButton);
+            Controls.Add(sButton);
+            Controls.Add(aButton);
+            Controls.Add(BackspaceButton);
+            Controls.Add(pButton);
+            Controls.Add(oButton);
+            Controls.Add(iButton);
+            Controls.Add(uButton);
+            Controls.Add(yButton);
+            Controls.Add(tButton);
+            Controls.Add(rButton);
+            Controls.Add(eButton);
+            Controls.Add(wButton);
+            Controls.Add(qButton);
+            Name = "Form1";
+            Text = "Keyboard";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button qButton;
+        private Button wButton;
+        private Button eButton;
+        private Button rButton;
+        private Button tButton;
+        private Button yButton;
+        private Button uButton;
+        private Button iButton;
+        private Button oButton;
+        private Button pButton;
+        private Button BackspaceButton;
+        private Button aButton;
+        private Button sButton;
+        private Button dButton;
+        private Button fButton;
+        private Button gButton;
+        private Button hButton;
+        private Button jButton;
+        private Button kButton;
+        private Button lButton;
+        private Button QuoteButton;
+        private Button EnterButton;
+        private Button zButton;
+        private Button xButton;
+        private Button cButton;
+        private Button bButton;
+        private Button vButton;
+        private Button nButton;
+        private Button mButton;
+        private Button CommaButton;
+        private Button DotButton;
+        private Button QuestionMarkButton;
+        private Button SpaceButton;
+        private TextBox Field;
+    }
+}
---
+++ Keyboard/Form1.cs
@@ -0,0 +1,68 @@
+using System.Text;
+
+namespace Keyboard
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Field.Text = "q";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Field_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
